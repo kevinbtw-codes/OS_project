f68b8c3af38124a69e6ebf050b5c859bbce4c932
Aryan Shah <aryanshah1902@gmail.com>
2021-03-01 22:11:25 +0530

    Learn page added

---
+++ lib/Learn pages/Learn.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'Widgets/FormCard.dart';
+import 'Widgets/SocialIcons.dart';
+import 'Widgets/CustomIcons.dart';
+
+void main() => runApp(MaterialApp(
+  home: LearnPage(),
+  debugShowCheckedModeBanner: false,
+));
+
+class LearnPage extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<LearnPage> {
+  bool _isSelected = false;
+
+  void _radio() {
+    setState(() {
+      _isSelected = !_isSelected;
+    });
+  }
+
+  Widget radioButton(bool isSelected) => Container(
+    width: 20,
+    height: 16.0,
+    padding: EdgeInsets.all(2.0),
+    decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(width: 2.0, color: Colors.black)),
+    child: isSelected
+        ? Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration:
+      BoxDecoration(shape: BoxShape.circle, color: Colors.black),
+    )
+        : Container(),
+  );
+
+  Widget horizontalLine() => Padding(
+    padding: EdgeInsets.symmetric(horizontal: 18.0),
+    child: Container(
+      width: ScreenUtil.getInstance().setWidth(120),
+      height: 1.0,
+      color: Colors.black26.withOpacity(.2),
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil.getInstance()..init(context);
+    ScreenUtil.instance =
+        ScreenUtil(width: 750, height: 1334, allowFontScaling: true);
+    return new Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomPadding: true,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 55.0),
+                //child: Image.asset("assets/image_01.png"),
+              ),
+              Expanded(
+                child: Container(),
+              ),
+              //Image.asset("assets/image_02.png")
+            ],
+          ),
+          SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(left: 28.0, right: 28.0, top: 60.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      /*Image.asset(
+                        "assets/logo.png",
+                        width: ScreenUtil.getInstance().setWidth(110),
+                        height: ScreenUtil.getInstance().setHeight(110),
+                      ),*/
+                      Text("FCFS",
+                          style: TextStyle(
+                              fontFamily: "Poppins-Bold",
+                              fontSize: ScreenUtil.getInstance().setSp(46),
+                              letterSpacing: .6,
+                              fontWeight: FontWeight.bold))
+                    ],
+                  ),
+                  SizedBox(
+                    height: ScreenUtil.getInstance().setHeight(180),
+                  ),
+                  FormCard(),
+                  SizedBox(height: ScreenUtil.getInstance().setHeight(40)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                    ],
+                  ),
+                  SizedBox(
+                    height: ScreenUtil.getInstance().setHeight(20),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      horizontalLine(),
+                      Text("FCFS",
+                          style: TextStyle(
+                              fontSize: 16.0, fontFamily: "Poppins-Medium")),
+                      horizontalLine()
+                    ],
+                  ),
+                  SizedBox(
+                    height: ScreenUtil.getInstance().setHeight(40),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+
+                    ],
+                  ),
+                  SizedBox(
+                    height: ScreenUtil.getInstance().setHeight(30),
+                  ),
+
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
